ac0008125a13e4e3a79c697ccf1f3338dca64b99
Irenee75014 <162982907+Irenee75014@users.noreply.github.com>
2024-03-11 12:50:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,3 @@
-import streamlit as st
-name=st.text_input("your name")
-st.write("Bonjour Monsieur  "+name)
-import streamlit as st
-
-genre = st.radio(
-    "Quel est ta matiere préféré ?",
-    [":rainbow[Chinois]", "***SNT***", "Math :"],
-    captions = ["Impossible.", "Tu mange des cartes graphique mon gars.", "1+1=3."])
-
-if genre == ':rainbow[Chinois]':
-    st.write('Sa ce voit t'est chinois.')
-else:
-    st.write("En vrai je suis d'accord avec toi.")
+input="lapin"
+list_possibilities=["rabbit","race","rhyme","rogne"]
+st.write("Traduis lapin.")

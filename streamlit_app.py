bd06f806c96faa6f9e109c9cd2dc87a9efc18c58
Irenee75014 <162982907+Irenee75014@users.noreply.github.com>
2024-03-11 12:23:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
 name=st.text_input("your name")
 st.write("Bonjour Monsieur  "+name)
-st.radio(label, options, index=0, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, horizontal=False, captions=None, label_visibility="visible")
+st.radio(label, options, index=0, format_func=special_internal_function, 
+key=None, help=None, on_change=None, args=None, kwargs=None, *,
+disabled=False, horizontal=False, captions=None, label_visibility="visible")

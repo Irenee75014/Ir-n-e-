b7e3dd369e5e8abc6e37e86fc1ce0b5888565862
Irenee75014 <162982907+Irenee75014@users.noreply.github.com>
2024-03-11 13:01:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 input="lapin"
 list_possibilities=["rabbit","race","rhyme","rogne"]
+correct_answer="rabbit"
 st.write(" Traduis :"+ input)
 for i in range(len(lost_possiblities)):
   st.button(list_possibilities[i])

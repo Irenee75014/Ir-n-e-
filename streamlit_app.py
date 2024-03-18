3df55299816f9422a665f2a12270983ec2038050
Irenee75014 <162982907+Irenee75014@users.noreply.github.com>
2024-03-18 11:42:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st 
 import pandas as pd
-voc=pd.nead_CSV('https://docs.google.com/spreadsheets/d/e/2PACX-1vSQ6MdH76cliOGXJgp2c3o7iEUQ9Z8bGhsCgc5H_DxAeZaB_52mdBCjASh-libSTDw_3pm_H5_QqtSg/pub?output=csv')
+voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSQ6MdH76cliOGXJgp2c3o7iEUQ9Z8bGhsCgc5H_DxAeZaB_52mdBCjASh-libSTDw_3pm_H5_QqtSg/pub?output=csv')
 st.dataframe(voc)

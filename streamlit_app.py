978adb1876bb834015d4ac52891e36b4ea11d1ef
Irenee75014 <162982907+Irenee75014@users.noreply.github.com>
2024-03-18 11:39:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,4 @@
-import streamlit as st
-input="lapin"
-list_possibilities=["rabbit","race","rhyme","rogne"]
-correct_answer="rabbit"
-st.write(" Traduis :"+ input)
-for i in range(len(list_possiblities)):
-  st.button(list_possibilities[i])
- 
+streamlit as st 
+import pandas as pd
+voc=pd.nead_CSV('https://docs.google.com/spreadsheets/d/e/2PACX-1vSQ6MdH76cliOGXJgp2c3o7iEUQ9Z8bGhsCgc5H_DxAeZaB_52mdBCjASh-libSTDw_3pm_H5_QqtSg/pub?output=csv')
+st.dataframe(voc)
